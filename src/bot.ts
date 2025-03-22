4e43b46fd4537fca35ebcef55a002f091d374be5
Adebayo Anuoluwa Success <adebayo.anuoluwa02@gmail.com>
2025-03-22 18:29:26 +0100

    add bot initialization and command handling with middleware integration

---
+++ src/bot.ts
@@ -0,0 +1,58 @@
+// Import necessary modules and utilities
+import { Telegraf } from "telegraf";
+
+import dotenv from "dotenv";
+import { authMiddleware } from "./middleware/authentication";
+import { rateLimitMiddleware } from "./middleware/rateLimit";
+import { errorHandler } from "./handlers/errorHandler";
+import { MyContext } from "./types/context";
+import { startCommand } from "./commands/start";
+import { helpCommand } from "./commands/help";
+import { authCommand, emailComand, myDataCommand } from "./commands/auth";
+import { kycCommand } from "./commands/kyc";
+import { sendCommand } from "./commands/send";
+import { walletCommand } from "./commands/wallet";
+import { depositCommand } from "./commands/deposit";
+import { beneficiaryCommand } from "./commands/beneficiary";
+import { transferCommand } from "./commands/transfer";
+
+// Load environment variables from .env file
+dotenv.config();
+
+export const initBot = () => {
+  // Initialize the Telegraf bot with the extended context
+  const bot = new Telegraf<MyContext>(process.env.TELEGRAM_BOT_TOKEN!, {
+    telegram: { agent: undefined, webhookReply: true },
+  });
+
+  /// Middleware to initialize session and check authentication
+  bot.use(authMiddleware, rateLimitMiddleware); // Apply rate limiting
+
+  // COMMANDS
+  startCommand(bot); // /start
+
+  helpCommand(bot); // /help
+
+  authCommand(bot); // /login
+
+  emailComand(bot); // /email
+
+  myDataCommand(bot); // /me
+
+  kycCommand(bot); // /kyc
+
+  sendCommand(bot); // /send
+
+  walletCommand(bot); // /wallets & /wallet
+
+  depositCommand(bot); // /deposit
+
+  beneficiaryCommand(bot); // /beneficiary, /addBeneficiary, /updateBeneficiary, /deleteBeneficiary
+
+  transferCommand(bot); // /transfer, /transfers
+
+  // Error handler
+  errorHandler(bot);
+
+  return bot;
+};
